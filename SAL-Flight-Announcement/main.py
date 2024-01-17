41d69fc4063a1bd769d9b52e9d389afb7280dbe1
TomBad13 <122915278+TomBad13@users.noreply.github.com>
2024-01-17 20:40:53 +0100

    Add files via upload

---
+++ SAL-Flight-Announcement/main.py
@@ -0,0 +1,13 @@
+#[NUMBER] [AUTOMATIC CALLSIGNS] [SAME DATE] [DATE]
+import sys,subprocess
+Number = sys.argv[1]
+if Number > 1:
+    AutoCallsign = sys.argv[2].upper()
+else:
+    AutoCallsign = "N"
+SameDate = sys.argv[3].upper()
+if SameDate == "Y":
+    Date = sys.argv[4]
+else:
+    Date = None
+subprocess.run(["python", "Execute.py", Number, AutoCallsign, SameDate, Date])
